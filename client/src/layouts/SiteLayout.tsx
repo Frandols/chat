1e186eaf825b64c4484c220d69561c54f1bef47f
Francisco <frandelosantoss12@gmail.com>
2023-06-26 17:35:33 -0300

    Set portfolio link rel to noreferrer

--- client/src/layouts/SiteLayout.tsx
+++ client/src/layouts/SiteLayout.tsx
@@ -14,7 +14,11 @@ const SiteLayout = ({ children }: SiteLayoutProps) => {
       <footer className="footer">
         <p className="footer-portfolio-link">
           Created by{' '}
-          <a href="https://portfolio-frandols.vercel.app/" target="_blank">
+          <a
+            href="https://portfolio-frandols.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Francisco De Los Santos
           </a>
         </p>
