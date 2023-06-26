4bfb3c65fa0e95340eea255a35270e10f17da302
Francisco <frandelosantoss12@gmail.com>
2023-06-26 17:26:09 -0300

    Add footer to layout

--- client/src/layouts/SiteLayout.tsx
+++ client/src/layouts/SiteLayout.tsx
@@ -1,7 +1,4 @@
-import type { 
-    ComponentProps, 
-    FC 
-} from 'react'
+import type { ComponentProps, FC } from 'react'
 
 import { Nav } from '../components'
 
@@ -10,15 +7,17 @@ import './SiteLayout.css'
 type SiteLayoutProps = ComponentProps<FC>
 
 const SiteLayout = ({ children }: SiteLayoutProps) => {
-    return (
-        <>  
-            <Nav/>
-            <main
-                className='main'>
-                {children}
-            </main>
-        </>
-    )
+  return (
+    <>
+      <Nav />
+      <main className="main">{children}</main>
+      <footer className="footer">
+        <p className="footer-portfolio-link">
+          Created by <a href="">Francisco De Los Santos</a>
+        </p>
+      </footer>
+    </>
+  )
 }
 
 export default SiteLayout
