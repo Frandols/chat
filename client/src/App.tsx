707ce8c0d75dd7db6b6e5bc05df954dcb3834baa
Francisco <frandelosantoss12@gmail.com>
2023-06-26 17:26:30 -0300

    Format code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,19 @@
 import { AnimatePresence } from 'framer-motion'
 
-import { 
-    Login, 
-    Chat 
-} from './components'
+import { Login, Chat } from './components'
 
 import { useSockets } from './context/socket'
 
 import './App.css'
 
 const App = () => {
-    const { user } = useSockets()
+  const { user } = useSockets()
 
-    return (
-        <AnimatePresence
-            mode='wait'>
-            {
-                user
-                ? <Chat key='chat' />
-                : <Login key='login' />
-            }
-        </AnimatePresence>
-    )
+  return (
+    <AnimatePresence mode="wait">
+      {user ? <Chat key="chat" /> : <Login key="login" />}
+    </AnimatePresence>
+  )
 }
 
 export default App
