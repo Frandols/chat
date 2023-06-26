20ae23e3dac384aede86b78ed12ce8c1699633bc
Francisco <frandelosantoss12@gmail.com>
2023-06-26 17:30:42 -0300

    Change chat asset format from png to avif

--- client/src/components/RoomItem.tsx
+++ client/src/components/RoomItem.tsx
@@ -2,45 +2,37 @@ import type { MouseEventHandler } from 'react'
 
 import { motion } from 'framer-motion'
 
-import chat from '../assets/chat.png'
+import chat from '../assets/chat.avif'
 
 import './RoomItem.css'
 
 import { RoomItemComponentAnimations } from '../animations'
 
 type RoomItemProps = {
-    index: number
-    name: string
-    onClick: MouseEventHandler<HTMLLIElement>
+  index: number
+  name: string
+  onClick: MouseEventHandler<HTMLLIElement>
 }
 
-const RoomItem = ({
-    index,
-    name,
-    onClick 
-}: RoomItemProps) => {
-    return (
-        <motion.li
-            className='room-item'
-            onClick={onClick}
-            variants={RoomItemComponentAnimations.item}
-            initial='hidden'
-            animate='visible'
-            transition={{
-                duration: .2,
-                type: 'spring',
-                damping: 30,
-                delay: index * .2
-            }}>
-            <img
-                src={chat}
-                alt='Chat Logo'
-                width={40}
-                height={40}
-            />
-            {name}
-        </motion.li>
-    )
+const RoomItem = ({ index, name, onClick }: RoomItemProps) => {
+  return (
+    <motion.li
+      className="room-item"
+      onClick={onClick}
+      variants={RoomItemComponentAnimations.item}
+      initial="hidden"
+      animate="visible"
+      transition={{
+        duration: 0.2,
+        type: 'spring',
+        damping: 30,
+        delay: index * 0.2,
+      }}
+    >
+      <img src={chat} alt="Chat Logo" width={40} height={40} />
+      {name}
+    </motion.li>
+  )
 }
 
 export default RoomItem
